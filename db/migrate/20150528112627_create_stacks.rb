f28465600dcddc03a0c70ccc930c54c2ccc6c146
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2015-05-28 13:45:37 +1200

    added seed file data, going to test before pushing, also added name column to stack table

--- db/migrate/20150528112627_create_stacks.rb
+++ db/migrate/20150528112627_create_stacks.rb
@@ -1,6 +1,7 @@
 class CreateStacks < ActiveRecord::Migration
   def change
     create_table :stacks do |t|
+      t.sting :name
       t.integer :user_id
       t.integer :facecard_id
       t.belongs_to :user, index: true
