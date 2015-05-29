76af10595583b355b46b672465df018105ec80fd
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2015-05-29 13:49:55 +1200

    added line of code in index controller so a new sign up gets access to the roa stack

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -53,6 +53,9 @@ end
 
 post '/sign_up' do
   user = User.create(email:params[:email], password:params[:password])
+  #we only have 1 stack at the moment, so this adds a new user to the roa stack
+  stack = user.stacks.find_or_create_by(name: "The Many Faces of Roa")
+
   if user
     session[:user] = user.id
     redirect '/'
